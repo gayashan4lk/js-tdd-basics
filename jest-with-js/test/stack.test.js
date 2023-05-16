62e6efab855b99971dc46d794a417dcbfb773c8e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-05-17 00:20:09 +0530

    write second test

--- jest-with-js/test/stack.test.js
+++ jest-with-js/test/stack.test.js
@@ -3,6 +3,15 @@ class Stack {
 		this.top = -1;
 		this.items = {};
 	}
+
+	push(item) {
+		this.top += 1;
+		this.items[this.top] = item;
+	}
+
+	get peek() {
+		return this.items[this.top];
+	}
 }
 
 describe('My stack', () => {
@@ -18,7 +27,17 @@ describe('My stack', () => {
 		expect(actual.items).toEqual({});
 	});
 
-	it.todo('should push new elements on the top of the stack');
+	it('should push new elements on the top of the stack', () => {
+		// Arrange
+		const stack = new Stack();
+
+		// Act
+		stack.push('👻');
+
+		// Assert
+		expect(stack.top).toBe(0);
+		expect(stack.peek).toEqual('👻');
+	});
 
 	it.todo('should pop the last element pushed into the stack');
 });
