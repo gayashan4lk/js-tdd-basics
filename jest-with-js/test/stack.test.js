5ca5a40424213b2664923813a69237b559af54e3
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-05-17 00:06:20 +0530

    create todo test cases

---
+++ jest-with-js/test/stack.test.js
@@ -0,0 +1,7 @@
+describe('My stack', () => {
+	it.todo('should be empty when created');
+
+	it.todo('should push new elements on the top of the stack');
+
+	it.todo('should pop the last element pushed into the stack');
+});
