14a9389e11239efe7f4bbd3759d346640a0832ea
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-05-17 00:13:24 +0530

    implement first test

--- jest-with-js/test/stack.test.js
+++ jest-with-js/test/stack.test.js
@@ -1,5 +1,22 @@
+class Stack {
+	constructor() {
+		this.top = -1;
+		this.items = {};
+	}
+}
+
 describe('My stack', () => {
-	it.todo('should be empty when created');
+	it('should be empty when created', () => {
+		// Arrange
+		const stack = new Stack();
+
+		// Act
+		const actual = stack;
+
+		// Assert
+		expect(actual.top).toBe(-1);
+		expect(actual.items).toEqual({});
+	});
 
 	it.todo('should push new elements on the top of the stack');
 
